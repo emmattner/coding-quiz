f88037d39eaf9d77c9e33ad6a62583cce16e5746
Emma Mattner <emattner1997@gmail.com>
2021-03-27 16:51:30 +1030

    adding quiz.js file

---
+++ quiz.js
@@ -0,0 +1,3 @@
+var pos = 0;
+var correct = 0;
+var test, test_status, question, choice, choices, chA, chB, chC;
--- script.js
+++ script.js
@@ -1,99 +1,106 @@
-var pos = 0;
-var correct = 0;
-var test, test_status, question, choice, choices, chA, chB, chC, chD;
+// function startQuiz(event){
+//     event.preventDefault();
+//     var myTimer = setInterval (function(){      
+//     time = time -0.10
+//        },10);
+//     }
 
-// this is a multidimensional array with 4 inner array elements with 5 elements inside them
-var questions = [
-    {
-        question: "The condition in an if / else statment is enclosed within _____.",
-        answers:{
-        a: "Quotes",
-        b: "Curly Brackets",
-        c: "Parentheses",
-        d: "Square Brackets",
-        },
-        correctAnswer: "C"
-      },
-    {
-        question: "String values must be enclosed within _____ when being assigned to varibles.",
-        answers: {
-        a: "Commas",
-        b: "Parentheses",
-        c: "Square Brackets",
-        d: "Quotes",
-        answer: "D"
-        },
-        correctAnswer: ""
-      },
-    {
-        question: "x:",
-        a: "Strings",
-        b: "Alerts",
-        c: "Booleans",
-        d: "Numbers",
-        answer: "B"
-      },
-    {
-        question: "What is 8 x 12?",
-        a: "88",
-        b: "112",
-        c: "96",
-        answer: "C"
-      }
-    ];
 
-    function get(x){
-        return document.getElementById(x);
-      }
-// this function renders a question for display on the page
-function renderQuestion(){
-    test = get("test");
-    if(pos >= questions.length){
-      test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-      get("test_status").innerHTML = "Test completed";
-      // resets the variable to allow users to restart the test
-      pos = 0;
-      correct = 0;
-      // stops rest of renderQuestion function running when test is completed
-      return false;
-    }
-    get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+// var pos = 0;
+// var correctAnswer = 0;
+// var test, test_status, question, choice, choices, chA, chB, chC, chD, correct;
+
+// var countOfCorrectAnswers = 0;
+// // this is a multidimensional array with 4 inner array elements with 5 elements inside them
+// var questions = [
+//     {
+//         question: "The condition in an if / else statment is enclosed within _____.",
+//         a: "Quotes",
+//         b: "Curly Brackets",
+//         c: "Parentheses",
+//         d: "Square Brackets",
+//         correctAnswer: "c"
+//       },
+//     {
+//         question: "String values must be enclosed within _____ when being assigned to varibles.",
+//         a: "Commas",
+//         b: "Parentheses",
+//         c: "Square Brackets",
+//         d: "Quotes",
+//         correctAnswer: "d"
+//       },
+//     {
+//         question: "Commonly used data types DO NOT include:",
+//         a: "Strings",
+//         b: "Alerts",
+//         c: "Booleans",
+//         d: "Numbers",
+//         correctAnswer: "b"
+//       },
+//     {
+//         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+//         a: "Console.log",
+//         b: "Javascript",
+//         c: "terminal/bash",
+//         d: "For loops",
+//         correctAnswer: "c"
+//       },
+//     ];
+
+//     function get(x){
+//         return document.getElementById(x);
+//       }
+// // this function renders a question for display on the page
+// function renderQuestion(){
+//     test = get("test");
+//     if(pos >= questions.length){
+//       test.innerHTML = "<h2>You got "+countOfCorrectAnswers+" of "+questions.length+" questions correct</h2>";
+//       get("test_status").innerHTML = "Test completed";
+//       // resets the variable to allow users to restart the test
+//       pos = 0;
+//       correct = 0;
+//       // stops rest of renderQuestion function running when test is completed
+//       return false;
+//     }
+//     get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
     
-    question = questions[pos].question;
-    chA = questions[pos].a;
-    chB = questions[pos].b;
-    chC = questions[pos].c;
-    // display the question
-    test.innerHTML = "<h3>"+question+"</h3>";
-    // display the answer options
-    // the += appends to the data we started on the line above
-    test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
-    test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
-    test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
-    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
-  }
+//     question = questions[pos].question;
+//     chA = questions[pos].a;
+//     chB = questions[pos].b;
+//     chC = questions[pos].c;
+//     chD = questions[pos].d;
+//     // display the question
+//     test.innerHTML = "<h3>"+question+"</h3>";
+//     // display the answer options
+//     // the += appends to the data we started on the line above
+//     test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+//     test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+//     test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br>";
+//     test.innerHTML += "<label> <input type='radio' name='choices' value='D'> "+chD+"</label><br><br>";
+//     test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+//   }
 
-  function checkAnswer(){
-    // use getElementsByName because we have an array which it will loop through
-    choices = document.getElementsByName("choices");
-    for(var i=0; i<choices.length; i++){
-      if(choices[i].checked){
-        choice = choices[i].value;
-      }
-    }
+//   function checkAnswer(){
+//     // use getElementsByName because we have an array which it will loop through
+//     choices = document.getElementsByName("choices");
+//     for(var i=0; i<choices.length; i++){
+//       if(choices[i].checked){
+//         choice = choices[i].value;
+//       }
+//     }
     // checks if answer matches the correct choice
-    if(choice == questions[pos].answer){
+    // if(choice == questions[pos].correctAnswer){
       //each time there is a correct answer this value increases
-      correct++;
-    }
+    //   countOfCorrectAnswers++;
+    // }
     // changes position of which character user is on
-    pos++;
+    // pos++;
     // then the renderQuestion function runs again to go to next question
-    renderQuestion();
-  }
+//     renderQuestion();
+//   }
 
 // Add event listener to call renderQuestion on page load event
-window.addEventListener("load", renderQuestion);
+// window.addEventListener("load", renderQuestion);
 
 // var button = 
 // document.createElement("button");
@@ -154,3 +161,87 @@ window.addEventListener("load", renderQuestion);
     
 // });
 // var timerEl = documentQuerySelector("#timer")
+// pos is position of where the user in the test or which question they're up to
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+// this is a multidimensional array with 4 inner array elements with 5 elements inside them
+var questions = [
+  {
+      question: "The condition in an if / else statment is enclosed within _____",
+      a: "Quotes",
+      b: "Parentheses",
+      c: "Curly Brackets",
+      answer: "B"
+    },
+  {
+      question: "String values must be enclosed within _____ when being assigned to varibles.",
+      a: "Sqaure Brackets",
+      b: "Commas",
+      c: "Quotes",
+      answer: "C"
+    },
+  {
+      question: "What is 16 / 4?",
+      a: "4",
+      b: "6",
+      c: "3",
+      answer: "A"
+    },
+  {
+      question: "What is 8 x 12?",
+      a: "88",
+      b: "112",
+      c: "96",
+      answer: "C"
+    }
+  ];
+// this get function is short for the getElementById function  
+function get(x){
+  return document.getElementById(x);
+}
+// this function renders a question for display on the page
+function renderQuestion(){
+  test = get("test");
+  if(pos >= questions.length){
+    test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+    get("test_status").innerHTML = "Test completed";
+    // resets the variable to allow users to restart the test
+    pos = 0;
+    correct = 0;
+    // stops rest of renderQuestion function running when test is completed
+    return false;
+  }
+  get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+  
+  question = questions[pos].question;
+  chA = questions[pos].a;
+  chB = questions[pos].b;
+  chC = questions[pos].c;
+  // display the question
+  test.innerHTML = "<h3>"+question+"</h3>";
+  // display the answer options
+  // the += appends to the data we started on the line above
+  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+  test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+function checkAnswer(){
+  // use getElementsByName because we have an array which it will loop through
+  choices = document.getElementsByName("choices");
+  for(var i=0; i<choices.length; i++){
+    if(choices[i].checked){
+      choice = choices[i].value;
+    }
+  }
+  // checks if answer matches the correct choice
+  if(choice == questions[pos].answer){
+    //each time there is a correct answer this value increases
+    correct++;
+  }
+  // changes position of which character user is on
+  pos++;
+  // then the renderQuestion function runs again to go to next question
+  renderQuestion();
+}
+// Add event listener to call renderQuestion on page load event
+window.addEventListener("load", renderQuestion);
